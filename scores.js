dd42fab8c8188d6881f26b965c95b2b31f97a436
Masoom Rizvi <105916377+masoom2313189@users.noreply.github.com>
2022-07-24 09:00:37 +0530

    Add files via upload

--- scores.js
+++ scores.js
@@ -1,17 +1,26 @@
-function slideShow(){
-    let image = ["https://tpc.googlesyndication.com/simgad/12185608401875927700?sqp=4sqPyQQ7QjkqNxABHQAAtEIgASgBMAk4A0DwkwlYAWBfcAKAAQGIAQGdAQAAgD-oAQGwAYCt4gS4AV_FAS2ynT4&rs=AOga4qmPjUjQlo3TTVGKg7TnmUYS8jAP1Q", "https://tpc.googlesyndication.com/simgad/4866054573033308061?", "https://tpc.googlesyndication.com/simgad/10926761020657163993?"]
-    let slide = document.getElementById("slideshow");
-    let i = 0;
+import navbar from "../navexport.js"
+document.getElementById("navbar").innerHTML= navbar();
 
-    setInterval(function(){
-        if(i === image.length){
-            i = 0;
-        }
+function slideShow() {
+  let image = [
+    "https://tpc.googlesyndication.com/simgad/12185608401875927700?sqp=4sqPyQQ7QjkqNxABHQAAtEIgASgBMAk4A0DwkwlYAWBfcAKAAQGIAQGdAQAAgD-oAQGwAYCt4gS4AV_FAS2ynT4&rs=AOga4qmPjUjQlo3TTVGKg7TnmUYS8jAP1Q",
+    "https://tpc.googlesyndication.com/simgad/4866054573033308061?",
+    "https://tpc.googlesyndication.com/simgad/10926761020657163993?",
+  ];
+  let slide = document.getElementById("slideshow");
+  let i = 0;
 
-        let img = document.createElement("img");
-        img.src = image[i];
-        slide.innerHTML = null;
-        slide.append(img);
-        i++;
-    }, 1000);
+  setInterval(function () {
+    if (i === image.length) {
+      i = 0;
+    }
+
+    let img = document.createElement("img");
+    img.src = image[i];
+    slide.innerHTML = null;
+    slide.append(img);
+    i++;
+  }, 1000);
 }
+
+ 
